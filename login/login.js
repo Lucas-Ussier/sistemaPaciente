70cd88d95eb28cb300db94c19bfe790bcae834a5
Lucas Galvao <lukedbzgalvao@gmail.com>
2024-07-10 18:45:37 -0300

    first commit

---
+++ login/login.js
@@ -0,0 +1,31 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Obtém os valores dos campos de entrada
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Limpa a mensagem de erro
+    document.getElementById('error-message').textContent = '';
+
+    // Valida os campos de entrada
+    if (username === '' || password === '') {
+        document.getElementById('error-message').textContent = 'Please fill in all fields.';
+    } else {
+        // Aqui você pode adicionar a lógica de autenticação
+        // Por exemplo, enviar os dados para um servidor para verificação
+        // e, se tudo estiver correto, redirecionar para uma nova página
+        if (username === 'admin' && password === 'admin') {
+            window.location.href = 'portalMedico.html';
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const userInput = prompt('Please enter a command:');
+    if (userInput) {
+        // Process the user input here
+        console.log('User entered command:', userInput);
+        alert('You entered: ' + userInput);
+    }
+});
